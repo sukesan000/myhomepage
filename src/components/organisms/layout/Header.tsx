92528af1f5da6f08fea9b53e4c356323eaa2e07f
sukesan000 <oyama.k.1997@gmail.com>
2021-07-22 17:12:02 +0900

    ヘッダーのボタンに遷移先を設定

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,7 +1,17 @@
 import { Box, Flex, Heading, Link } from '@chakra-ui/react'
-import { memo, VFC } from 'react'
+import { memo, useCallback, VFC } from 'react'
+import { useHistory } from 'react-router-dom'
 
 export const Header: VFC = memo(() => {
+  const history = useHistory()
+  const onClickProfile = useCallback(() => history.push('/'), [history])
+  const onClickServices = useCallback(
+    () => history.push('/services'),
+    [history],
+  )
+  const onClickSocial = useCallback(() => history.push('/social'), [history])
+  const onClickWorks = useCallback(() => history.push('/works'), [history])
+
   return (
     <Flex
       as="nav"
@@ -11,22 +21,28 @@ export const Header: VFC = memo(() => {
       color="gray.100"
       padding={{ base: 3, md: 5 }}
     >
-      <Flex align="center" as="a" mr={8} _hover={{ cursor: 'pointer' }}>
+      <Flex
+        align="center"
+        as="a"
+        mr={8}
+        _hover={{ cursor: 'pointer' }}
+        onClick={onClickProfile}
+      >
         <Heading as="h1" fontSize={{ base: 'md', md: 'lg' }}>
           わいのホームページ
         </Heading>
       </Flex>
       <Flex fontSize="sm" align="center" display={{ base: 'none', md: 'flex' }}>
         <Box pr={4}>
-          <Link>Profile</Link>
+          <Link onClick={onClickProfile}>Profile</Link>
         </Box>
         <Box pr={4}>
-          <Link>Services</Link>
+          <Link onClick={onClickServices}>Services</Link>
         </Box>
         <Box pr={4}>
-          <Link>Social</Link>
+          <Link onClick={onClickSocial}>Social</Link>
         </Box>
-        <Link>Works</Link>
+        <Link onClick={onClickWorks}>Works</Link>
       </Flex>
     </Flex>
   )
